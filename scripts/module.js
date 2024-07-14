e349250d9b42eb7ce61b523e7b1ee4a6b177ec63
BadIdeasBureau <dumbmuscle@gmail.com>
2024-07-14 11:24:25 +0100

    Reintroducing localisation

--- scripts/module.js
+++ scripts/module.js
@@ -1,34 +1,34 @@
 const structTableTitles = [
-  "Crushing Hit",
-  "Direct Hit",
-  "System Trauma",
-  "System Trauma",
-  "System Trauma",
-  "Glancing Blow",
-  "Glancing Blow",
+  game.i18n.localize("LANCER-ALT-STRUCTURE.StructureTitles.crushingHit"),
+  game.i18n.localize("LANCER-ALT-STRUCTURE.StructureTitles.directHit"),
+  game.i18n.localize("LANCER-ALT-STRUCTURE.StructureTitles.systemTrauma"),
+  game.i18n.localize("LANCER-ALT-STRUCTURE.StructureTitles.systemTrauma"),
+  game.i18n.localize("LANCER-ALT-STRUCTURE.StructureTitles.systemTrauma"),
+  game.i18n.localize("LANCER-ALT-STRUCTURE.StructureTitles.glancingBlow"),
+  game.i18n.localize("LANCER-ALT-STRUCTURE.StructureTitles.glancingBlow"),
 ];
 
 function structTableDescriptions(roll, remStruct) {
   switch (roll) {
     // Used for multiple ones
     case 0:
-      return "Roll a HULL check. On a success, until the end of your next turn, your mech cannot take its standard move, reactions, or free actions of any kind, including protocols, and during your next turn, you can only take 1 quick action. On a Failure, your mech is immediately destroyed.";
+      return game.i18n.localize("LANCER-ALT-STRUCTURE.StructureDescriptions.crushingHit");
     case 1:
       switch (remStruct) {
         case 2:
-          return "Roll a HULL check. On a success, your mech is @Compendium[world.status-items.Impaired] and @Compendium[world.status-items.Slowed] until the end of your next turn. On a failure, you take the SYSTEM TRAUMA result from this table, and your mech is @Compendium[world.status-items.Impaired] and @Compendium[world.status-items.Immobilized] until the end of your next turn. If there are no valid systems or weapons remaining, this result becomes a CRUSHING HIT instead.";
+          return game.i18n.localize("LANCER-ALT-STRUCTURE.StructureDescriptions.directHit.two");
         case 1:
-          return "You take the SYSTEM TRAUMA result from this table and must roll a HULL check. On a success, your mech is @Compendium[world.status-items.Impaired] and @Compendium[world.status-items.Slowed] until the end of your next turn. On a Failure, Your mech is @Compendium[world.status-items.Stunned] until the end of your next turn. If there are no valid weapons or systems remaining, this result becomes a CRUSHING HIT instead.";
+          return game.i18n.localize("LANCER-ALT-STRUCTURE.StructureDescriptions.directHit.one");
         default:
-          return "Your mech is @Compendium[world.status-items.Stunned] until the end of your next turn.";
+          return game.i18n.localize("LANCER-ALT-STRUCTURE.StructureDescriptions.directHit.base");
       }
     case 2:
     case 3:
     case 4:
-      return "Parts of your mech are torn off by the damage. Roll 1d6. On a 1–3, all weapons on one mount of your choice are destroyed; on a 4–6, a system of your choice is destroyed. LIMITED systems and weapons that are out of charges are not valid choices. If there are no valid choices remaining, it becomes the other result. If there are no valid systems or weapons remaining, this result becomes a DIRECT HIT instead.";
+      return game.i18n.localize("LANCER-ALT-STRUCTURE.StructureDescriptions.systemTrauma");
     case 5:
     case 6:
-      return "Emergency systems kick in and stabilize your mech, but it’s @Compendium[world.status-items.Impaired] until the end of your next turn.";
+      return game.i18n.localize("LANCER-ALT-STRUCTURE.StructureDescriptions.glancingBlow");
   }
   return "";
 }
